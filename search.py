fb19232f44fbee550599f2ff1cc7d9456a723bb7
satomi-ueda21 <gionkuri@hotmail.com>
2021-12-02 17:54:55 +0900

    task1-3リスト化

--- search.py
+++ search.py
@@ -1,21 +1,33 @@
 #検索ソース
 ### 検索ツールサンプル
 ### これをベースに課題の内容を追記してください
+import csv
 
 # 検索ソース
-source=["ねずこ","たんじろう","きょうじゅろう","ぎゆう","げんや","かなお","ぜんいつ"]
+source = []
+
+with open('task1.csv', encoding="utf-8") as f:
+  date_reader = csv.reader(f)
+
+  for row in date_reader:
+    source.append(row[0])
+
+print(source)
+print(date_reader)
+
+# source=["ねずこ","たんじろう","きょうじゅろう","ぎゆう","げんや","かなお","ぜんいつ"]
 
 ### 検索ツール
-def search():
-    word =input("鬼滅の登場人物の名前を入力してください >>> ")
+# def search():
+#     word =input("鬼滅の登場人物の名前を入力してください >>> ")
 
-    ### ここに検索ロジックを書く
-    if word in source:
-      print("{}が見つかりした".format(word))
-    else:
-      source.append(word)
-      print("リストに{}は存在しません。新しく追加します。".format(word))
-      print(source)
+#     ### ここに検索ロジックを書く
+#     if word in source:
+#       print("{}が見つかりした".format(word))
+#     else:
+#       source.append(word)
+#       print("リストに{}は存在しません。新しく追加します。".format(word))
+#       print(source)
 
-if __name__ == "__main__":
-    search()
+# if __name__ == "__main__":
+#     search()
